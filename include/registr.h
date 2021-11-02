48bb4db7598af04e8f0967da789a296086ccea5a
-=Oktan=- <r.zhigalenko@mail.ru>
2021-11-03 01:03:17 +0500

    completed off -Wfloat-equal (bug)

--- include/registr.h
+++ include/registr.h
@@ -16,7 +16,33 @@ public:
         Pair() : _key(K()), _value(V()) {}
     };
     
-private:    std::vector<Pair<T1, T2>> _reg;
+private:    
+    std::vector<Pair<T1, T2>> _reg;
+    int binary_get_index(const T1 &key) const
+    {
+        if (_reg.empty())
+            return -1;
+
+        int low = 0;
+        int high = static_cast<int>(_reg.size());
+        high -= 1;
+        while (low <= high)
+        {
+            int midle = (low + high) / 2;
+            Pair<T1, T2> el = _reg[static_cast<std::size_t>(midle)];
+            if (el._key == key)
+                return midle;
+            if (el._key > key)
+            {
+                high = midle - 1;
+            }
+            else
+            {
+                low = midle + 1;
+            }
+        }
+        return -1;
+    }
 
 public:
     Registr(){}
@@ -50,5 +76,20 @@ public:
         for (const Pair<T1, T2> &pair : _reg)
             std::cout << pair._key << " " << pair._value << std::endl;
     }
+
+    T2 find(const T1& key) const
+    {
+        int index = binary_get_index(key);
+        if(index == -1)
+            return T2();
+        return _reg[static_cast<std::size_t>(index)]._value;
+    }
+
+    void remove (const T1& key)
+    {
+        int index = binary_get_index(key);
+        _reg.erase(_reg.begin() + index);
+    }
+
 };
 
